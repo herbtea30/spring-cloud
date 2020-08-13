d7afd35d046e6e8485e5f26ed929dbbb1542e4e7
herbtea <herbtea30@gmail.com>
2020-08-13 22:05:20 +0900

    Spring cloud stream rabbitmq

--- msa-review-api/src/main/java/api/review/msa/controller/ReviewController.java
+++ msa-review-api/src/main/java/api/review/msa/controller/ReviewController.java
@@ -1,13 +1,28 @@
 package api.review.msa.controller;
 
+import api.review.msa.component.MessageProducer.WriteReviewSource;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReviewController {
 
+    private WriteReviewSource writeReviewSource;
+
+    public ReviewController(WriteReviewSource writeReviewSource) {
+        this.writeReviewSource = writeReviewSource;
+    }
+
     @GetMapping("/")
     public String getReview() {
         return "review Information";
+    }
+
+    @PostMapping("/")
+    public String setReview() {
+        writeReviewSource.writeReview().send(MessageBuilder.withPayload("{seq : 13322}").build());
+        return "write review";
     }
 }
